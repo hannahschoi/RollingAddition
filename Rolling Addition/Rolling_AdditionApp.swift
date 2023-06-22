896132bf4f1114f7f39b9842afaa24bc88f2d3b5
Hannah Choi <hannahchoi2006@gmail.com>
2023-06-21 23:16:25 -0700

    Initial Commit

---
+++ Rolling Addition/Rolling_AdditionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rolling_AdditionApp.swift
+//  Rolling Addition
+//
+//  Created by Hannah Choi on 2023-06-21.
+//
+
+import SwiftUI
+
+@main
+struct Rolling_AdditionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
